a1fc40999a88c1a3ee5e559a95072c145bbf9939
dshmagin <dmytro.shmagin@gmail.com>
2019-02-03 17:29:20 -0500

    Added CMake functionality and finished original implemetation of whoami

--- src/whoami.c
+++ src/whoami.c
@@ -1,1 +1,17 @@
-# whoami implementation
+// whoami implementation
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pwd.h>
+
+int whoami(){
+	int uid = getuid();
+	printf("%s\n", getpwuid(uid)->pw_name);
+	return EXIT_SUCCESS;
+}
+
+int main(int argc, char** argv){
+	whoami();
+	return 0;
+}
