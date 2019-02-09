3d793f4ee5e76f34f3b4031cde8ba86366fc1cd0
dshmagin <dmytro.shmagin@gmail.com>
2019-02-09 17:12:41 -0500

    Added -d functionality (without arguments)

--- src/ls.c
+++ src/ls.c
@@ -5,12 +5,19 @@
 int ls(){
 	char* dirname = getenv("PWD");
 	DIR *current_dir = opendir(dirname);
-	struct dirent *dir_to_list  = readdir(current_dir);
 
 	struct dirent *current_file;
 	while ((current_file = readdir(current_dir)) != NULL){
-		printf("  %s\n", current_file->d_name);
+		//if(current_file->d_name[0] != '.'){
+		//	printf("  %s\n", current_file->d_name);
+		//}
+		if(current_file->d_type == DT_DIR){
+			printf("IT'S A DIRECTORY: %s\n", current_file->d_name);
+		} else {
+			printf("    %s\n", current_file->d_name);
+		}
 	}
+	closedir(current_dir);
 }
 
 int main(int argc, char* argv){
