3a87c442eb8a68235d25fa7427f45b45ae546c2e
dshmagin <dmytro.shmagin@gmail.com>
2019-02-09 16:56:52 -0500

    Added basic functionality to ls()

--- src/ls.c
+++ src/ls.c
@@ -1,3 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<dirent.h>
 
+int ls(){
+	char* dirname = getenv("PWD");
+	DIR *current_dir = opendir(dirname);
+	struct dirent *dir_to_list  = readdir(current_dir);
 
+	struct dirent *current_file;
+	while ((current_file = readdir(current_dir)) != NULL){
+		printf("  %s\n", current_file->d_name);
+	}
+}
+
+int main(int argc, char* argv){
+	ls();
+	return 0;
+}
