7534b33cc1133c7300fc54d668c7ffc64041d65f
SabrinaSammel <smsammel@email.wm.edu>
2019-02-09 18:19:31 -0500

    Update ls.c

--- src/ls.c
+++ src/ls.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<dirent.h>
+#include<getopt.h>
 
 int ls(){
 	char* dirname = getenv("PWD");
@@ -14,6 +15,44 @@ int ls(){
 }
 
 int main(int argc, char* argv){
+	
+	static struct option full_arg[] =   //building a struct to house all of the written out command args 
+	{
+    	    {"all", required_argument, NULL, 'a'},
+    	    {"directory", required_argument, NULL, 'd'},
+	    {"group-directories-first", required_arugment, NULL, 1}
+	    {"reverse", required_argument, NULL, r}
+	    {"recursive", required_argument, NULL, R}
+    	    {NULL, 0, NULL, 0}
+	};
+	
+	while ((sarg = getopt (argc, argv, "a:d:l:r:R:S:t:full_arg")) != -1) {
+		switch (sarg) 
+		    {
+		    case 'a':
+		    break;
+		    
+                    case 'd':
+		    break;
+			
+		    case 'l':
+	            break;
+			
+		    case 'r':
+		    break;
+				
+		    case 'R':
+		    break;
+				
+		    case 'S':
+		    break;
+				
+		    case 't':
+		    break;
+				
+			
+
+	
 	ls();
 	return 0;
 }
