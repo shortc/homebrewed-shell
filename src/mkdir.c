b794b296a5cc62a2e224fce9ef92a2ac6e086c32
SabrinaSammel <smsammel@email.wm.edu>
2019-02-13 23:57:43 -0500

    Functional Mkdir

--- src/mkdir.c
+++ src/mkdir.c
@@ -5,113 +5,144 @@
 #include<sys/stat.h>
 #include<dirent.h>
 #include<sys/types.h>
-#include <pwd.h>
-#include <grp.h>
-#include <time.h>
+#include<pwd.h>
+#include<grp.h>
+#include<time.h>
 #include<string.h>
-	
+
+int parent(int argc, char**argv, char* dirname)
+{
+  printf("We used only the parent argument\n");
+        int j = 0;
+        int i = 0;
+        char str[40];
+        strcpy(str, argv[optind]);
+        char *file_ptr = strtok(str, "/");
+        char *array[5];
+
+        while (file_ptr != NULL)
+        {
+            array[i++] = file_ptr;
+            file_ptr = strtok (NULL, "/");
+        }
+        char* OG_dir = dirname;
+        char slash[] = "/";
+        DIR *current_dir;
+
+        while (j < i)
+          {
+            printf("We are in the loop.\n");
+            mkdir(array[j], S_IRWXU | S_IRWXG | S_IRWXO);
+        //    printf("Hopefully we made some directories\n");
+
+            strcat(dirname, slash);
+            printf("%s\n", dirname);
+            strcat(dirname, array[j]);
+            printf("%s\n", dirname);
+
+            chdir(dirname);
+
+            current_dir = opendir(dirname);                   //going one directory down in theory
+            j++;
+          }
+    return 0;
+}
+
+
+int orphan(int argc, char** argv)
+{
+  printf("We have not used any argument\n");
+  if (optind < argc)
+      printf("In theory, these will be our files: \n");
+      while (optind < argc)
+      {
+          printf ("%s\n", argv[optind]);
+          mkdir(argv[optind], S_IRWXU | S_IRWXG | S_IRWXO);
+          optind++;
+      }
+    return 0;
+}
+
+
+
+int loud(int argc, char** argv)
+{
+  printf("We used only the verbose argument\n");
+
+    if (optind < argc)
+      printf("In theory, these will be our files: \n");
+    while (optind < argc)
+    {
+      printf ("%s\n", argv[optind]);
+      mkdir(argv[optind], S_IRWXU | S_IRWXG | S_IRWXO);
+      printf("mkdir: created directory '%s' \n", argv[optind]);
+      optind++;
+    }
+  return 0;
+}
+
+
 
 int main(int argc, char** argv)
 {
-	
-	int command_flag[2];                                 //argument flag	
-	int command_index = 0;
-        int no_of_files = 0;
-        int sarg;
+    int command_flag[2];                                   //argument flag
+    int command_index = 0;
+    int sarg;
 
-	char* dirname = getenv("PWD");                              //find the directories pathname 
-	DIR *current_dir = opendir(dirname);                        //opening our current directory (this will be our default)
-	struct dirent *dir_to_list  = readdir(current_dir);         //next file in directory, will tell us if dir already exists?
-				
-	while (1)
-	{
-		static struct option full_arg[] =                   //building a struct to house all of the written out command args
-		{
-			{"parent", 0, 0, 'p'},
-			{"verbose", 0, 0, 'v'},
-			{NULL, 0, NULL, 0}
-		};
 
-		sarg = getopt_long(argc, argv, "p:v", full_arg, &command_index);
+    char* dirname = getenv("PWD");                         //find the directories pathname
+    DIR *current_dir = opendir(dirname);                   //opening our current directory (this will be our default)
+    struct dirent *dir_to_list  = readdir(current_dir);    //next file in directory, will tell us if dir already exists?
 
-		if (sarg == -1)                                    //if there are no/no more arguments 
-		{
-			break;
-		}
-	
-		switch (sarg)
-		{
-			case 0
-              			if (full_arg[command_index].flag != 0)
-					break;
-                		printf ("option %s", full_arg[command_index].name);
-                		if (optarg)
-					printf (" with arg %s", optarg);
-                			printf ("\n");
-               				 break;                            
-            		case 'p':                                  //-p --parent
-            			command_flag[0] = 1;
-                		puts ("option -p\n");
-                		break;
-            		case 'v':                                  //-v --verbose
-                		command_flag[1] = 1;
-                		puts ("option -v\n");
-                		break;	
-		}
-	}
-	/*Tell me if we used either argument*/
-	printf("%d",command_flag[0]);
-	printf("%d",command_flag[1]);
-	
-	   
-         /*This will be used to read out the file names*/
-	char file_arr[255];
-	int file_count = 0;
-	int curr_optind;
-	if (optind < argc) 
-	{
-        	printf("In theory, these will be our files: ");
-
-        	while (optind < argc)
-		{
-			curr_optind = optind;
-            		printf ("%s ", argv[curr_optind]);
-			file_arr[file_count] = curr_optind;
-			optind++; 
-			file_count ++;
-		}
-        	putchar ('\n');
-   	 }
-	
-/*NEW STUFF*/
-    int i = 0;
-    char *s = strtok(file_arr, "/");
-    char *file_list[255];
-
-    while(s != NULL)
+    while (1)
     {
-        file_list[i++] = s;
-        s = strtok(NULL, "/");
+        static struct option full_arg[] =              //building a struct to house all of the written out command args
+        {
+            {"parent", 0, 0, 'p'},
+            {"verbose", 0, 0, 'v'},
+            {NULL, 0, NULL, 0}
+        };
+                   
+        sarg = getopt_long(argc, argv, "pv", full_arg, &command_index);
+            if (sarg == -1)                                //if there are no/no more arguments
+                break;
+
+            switch (sarg)
+            {
+                case 0:
+                    break;
+
+                case 'p':                                  //-p --parent
+                    command_flag[0] = 1;
+                    puts ("option -p\n");
+                    break;
+
+                case 'v':                                  //-v --verbose
+                    command_flag[1] = 1;
+                    puts ("option -v\n");
+                    break;
+            }
     }
-    for (i = 0; i < sizeof(file_list)/sizeof(file_list[0]); ++i) 
-        printf("%s\n", file_list[i]);
-/*NEW STUFF*/
-	
-	if (command_flag[0] == 1)
-	{
-		printf("We used the parent argument");
-		//for (i = 0; i < sizeof(file_list)/sizeof(file_list[0]); ++i) 
-			
-	}
-	
-	if (command_flag[1] == 1)
-	{
-		printf("We used the verbose argument");
-		for (i = 0; i < sizeof(file_list)/sizeof(file_list[0]); ++i) 
-			const char *file = file_list[i];
-			int mkdir(file, S_IRWXU | S_IRWXG | S_IRWXO );
-			printf("mkdir: created directory '%s'", file_list[i]);
-	}
-	
-	return 0;
+
+    /*Tell me if we used either argument*/
+    printf("%d\n",command_flag[0]);
+    printf("%d\n",command_flag[1]);
+
+    if(command_flag[0] == 0 && command_flag[1] == 0)
+    {
+      orphan(argc, argv);
+    }
+
+    if(command_flag[0] == 1 && command_flag[1] == 0)
+    {
+      parent(argc, argv, dirname);
+    }
+
+    if (command_flag[0] == 0 && command_flag[1] == 1)
+    {
+       loud(argc,  argv);
+    }
+
+    return 0;
 }
+                                        	    
